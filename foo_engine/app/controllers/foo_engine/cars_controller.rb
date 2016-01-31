3a1f6553f96b02bad353bc4e51e3a3150008ae6b
Kelly Stannard <kelly.stannard@caspersleep.com>
2016-01-31 13:45:40 -0500

    add a simple form

--- foo_engine/app/controllers/foo_engine/cars_controller.rb
+++ foo_engine/app/controllers/foo_engine/cars_controller.rb
@@ -1,5 +1,13 @@
-class CarsController < ApplicationController
-  def index
-    @cars = Car.all
+module FooEngine
+  class CarsController < ApplicationController
+    def index
+      @cars = Car.all
+    end
+
+    def create
+      Car.create!(params.permit(:make, :model))
+
+      redirect_to action: :index
+    end
   end
 end
